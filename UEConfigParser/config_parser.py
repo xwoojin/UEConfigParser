f33011656e729d774999c250dd9bc4cd8157dfaa
WooJin Kim <33077491+xwoojin@users.noreply.github.com>
2025-01-15 11:12:40 +0900

    Update config_parser.py
    added replace functionality

--- UEConfigParser/config_parser.py
+++ UEConfigParser/config_parser.py
@@ -145,7 +145,7 @@ class UnrealConfigParser:
         :param key: Key name to disable
         """
         in_section = False
-        key_found = False
+        commented = False
         exists = False
         updated_lines = []
         for line in self.lines:
@@ -158,22 +158,23 @@ class UnrealConfigParser:
                 current_key, value = map(str.strip, stripped.split('=', 1))
                 if current_key == key:
                     line = f'; {line}'
-                    key_found = True
+                    exists = True
             if in_section and '=' in stripped:
                 current_key, value = map(str.strip, stripped.split('=', 1))
                 if current_key == key:
-                    exists = True
-
+                    if stripped.startswith(';') or stripped.startswith('#'):
+                        commented = True
             updated_lines.append(line)
 
+        print(f'in Section: {section}')
+        print(f'in Key: {key}')
         if exists:
-            if not key_found:
-                print(f'{section} - {key} : is already disabled')
+            if commented:
+                print(f'Commented')
             else:
-                print(f'{section} - {key} : commented')
+                print(f'Already commented')
         else:
-            print(f'{section} - {key} : does not exist')
-
+            print(f'Does not exist')
         self.lines = updated_lines
 
     def uncomment_key(self, section: str, key: str):
@@ -183,8 +184,8 @@ class UnrealConfigParser:
         :param key: Key name to enable
         """
         in_section = False
-        key_found = False
         exists = False
+        commented = False
         updated_lines = []
         for line in self.lines:
             stripped = line.strip()
@@ -197,22 +198,363 @@ class UnrealConfigParser:
                 current_key, value = map(str.strip, uncommented_line.split('=', 1))
                 if current_key == key:
                     line = uncommented_line + '\n'
-                    key_found = True
+                    exists = True
             if in_section and '=' in stripped:
                 uncommented_line = stripped[1:].strip()
                 current_key, value = map(str.strip, uncommented_line.split('=', 1))
                 if current_key == key:
-                    line = uncommented_line + '\n'
+                    if stripped.startswith(';') or stripped.startswith('#'):
+                        commented = True
+            updated_lines.append(line)
+
+        print(f'in Section: {section}')
+        print(f'in Key: {key}')
+        if exists:
+            if commented:
+                print(f'Already uncommented')
+            else:
+                print(f'Uncommented')
+        else:
+            print(f'Does not exist')
+        self.lines = updated_lines
+
+    def update_value_if_matches_in_section(self, section: str, match_substring: str, new_value: str):
+        """
+        Updates the value of any key in the given section if the full 'key=value' string contains the match_substring.
+    
+        :param section: The section to search in.
+        :param match_substring: The substring to match within the 'key=value' string.
+        :param new_value: The new value to set if the substring matches.
+        """
+        in_section = False
+        updated_lines = []
+        exists = False
+        active_key = ''
+        active_value = ''
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and '=' in stripped and not stripped.startswith((';', '#')):
+                key, value = map(str.strip, stripped.split('=', 1))
+                if match_substring in stripped:
+                    line = f'{key}={new_value}\n'
+                    active_key = key
+                    active_value = new_value
                     exists = True
             updated_lines.append(line)
-        if exists:
-            if not key_found:
-                print(f'{section} - {key} : is already enabled')
-            else:
-                print(f'{section} - {key} : uncommented')
-        else:
-            print(f'{section} - {key} : does not exist')
+    
+        print('Search Substring:', match_substring)
+        print(f'in Section: {section}')
 
+        if not exists:
+            print('NOT FOUND')
+        else:
+            print('Search Substring Found.')
+            print('New record is as follows')
+            print(f'{active_key}={active_value}')
+        self.lines = updated_lines
+
+    def update_value_if_matches(self, section: str, key: str, match_substring: str, new_value: str):
+        """
+        Updates the value of a specific key in a section if the current value contains the match_substring.
+        
+        :param section: The section to search in.
+        :param key: The key whose value needs to be updated.
+        :param match_substring: The substring to match in the current value.
+        :param new_value: The new value to set if the substring matches.
+        """
+        in_section = False
+        updated_lines = []
+        exists = False
+        active_key = ''
+        active_value = ''
+        
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and '=' in stripped and not stripped.startswith((';', '#')):
+                current_key, value = map(str.strip, stripped.split('=', 1))
+                if current_key == key:
+                    exists = True
+                    if match_substring in value:
+                        line = f'{key}={new_value}\n'
+                        active_key = key
+                        active_value = new_value
+            updated_lines.append(line)
+
+        print(f'Search Substring: {match_substring}')
+        print(f'in Section: {section}')
+        print(f'in Key: {key}')
+
+        if not exists:
+            print('NOT FOUND')
+        else:
+            print('Search Substring Found.')
+            print('New record is as follows')
+            print(f'{active_key}={active_value}')
+        self.lines = updated_lines
+
+    def comment_key_if_value_matches(self, section: str, key: str, match_substring: str):
+        """
+        comment entire key if value is matched in given section/key
+        
+        :param section: The section to search in.
+        :param key: The key whose value needs to be updated.
+        :param match_substring: The substring to match in the current value.
+        """
+        in_section = False
+        exists = False
+        commented = False
+        updated_lines = []
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_target_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and '=' in stripped and not stripped.startswith((';', '#')):
+                current_key, value = map(str.strip, stripped.split('=', 1))
+                if current_key == key and match_substring in value:
+                    line = f'; {line}'
+                    exists = True
+            if in_section and '=' in stripped:
+                current_key, value = map(str.strip, stripped.split('=', 1))
+                if current_key == key and match_substring in value:
+                    if stripped.startswith(';') or stripped.startswith('#'):
+                        commented = True
+            updated_lines.append(line)
+        
+        print(f'Search Substring: {match_substring}')
+        print(f'in Section: {section}')
+        print(f'in Key: {key}')
+        if exists:
+            if commented:
+                print(f'Already commented')
+            else:
+                print(f'Commented')
+        else:
+            print(f'Does not exist')
+        self.lines = updated_lines
+
+    def uncomment_key_if_value_matches(self, section: str, key: str, match_substring: str):
+        """
+        uncomment entire key if value is matched in given section/key
+        
+        :param section: The section to search in.
+        :param key: The key whose value needs to be updated.
+        :param match_substring: The substring to match in the current value.
+        """
+        in_section = False
+        exists = False
+        commented = False
+        updated_lines = []
+
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_target_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and stripped.startswith(';') and '=' in stripped:
+                uncommented_line = stripped[1:].strip()
+                current_key, value = map(str.strip, uncommented_line.split('=', 1))
+                if current_key == key and match_substring in value:
+                    line = uncommented_line + '\n'
+                    exists = True
+            if in_section and '=' in stripped:
+                uncommented_line = stripped[1:].strip()
+                current_key, value = map(str.strip, uncommented_line.split('=', 1))
+                if current_key == key and match_substring in value:
+                    if stripped.startswith(';') or stripped.startswith('#'):
+                        commented = True
+            updated_lines.append(line)
+        
+        print(f'Search Substring: {match_substring}')
+        print(f'in Section: {section}')
+        print(f'in Key: {key}')
+        if exists:
+            if commented:
+                print(f'Uncommented')
+            else:
+                print(f'Already uncommented')
+        else:
+            print(f'Does not exist')
+        self.lines = updated_lines
+
+    def comment_key_if_value_matches_in_section(self, section: str, match_substring: str):
+        """
+        comment entire key if value is matched in given section
+        
+        :param section: The section to search in.
+        :param key: The key whose value needs to be updated.
+        :param match_substring: The substring to match in the current value.
+        """
+        in_section = False
+        exists = False
+        commented = False
+        updated_lines = []
+
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_target_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and '=' in stripped and not stripped.startswith((';', '#')):
+                current_key, value = map(str.strip, stripped.split('=', 1))
+                if match_substring in value:
+                    line = f'; {line}'
+                    exists = True
+            if in_section and '=' in stripped:
+                current_key, value = map(str.strip, stripped.split('=', 1))
+                if match_substring in value:
+                    if stripped.startswith(';') or stripped.startswith('#'):
+                        commented = True
+            updated_lines.append(line)
+
+        print(f'Search Substring: {match_substring}')
+        print(f'in Section: {section}')
+        if exists:
+            if commented:
+                print(f'Already commented')
+            else:
+                print(f'Commented')
+        else:
+            print(f'Does not exist')
+        self.lines = updated_lines
+
+    def uncomment_key_if_value_matches_in_section(self, section: str, match_substring: str):
+        """
+        uncomment entire key if value is matched in given section
+        
+        :param section: The section to search in.
+        :param match_substring: The substring to match in the current value.
+        """
+        in_section = False
+        exists = False
+        commented = False
+        updated_lines = []
+
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_target_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and stripped.startswith(';') and '=' in stripped:
+                uncommented_line = stripped[1:].strip()
+                current_key, value = map(str.strip, uncommented_line.split('=', 1))
+                if match_substring in value:
+                    line = uncommented_line + '\n'
+                    exists = True
+            if in_section and '=' in stripped:
+                uncommented_line = stripped[1:].strip()
+                current_key, value = map(str.strip, uncommented_line.split('=', 1))
+                if match_substring in value:
+                    if stripped.startswith(';') or stripped.startswith('#'):
+                        commented = True
+            updated_lines.append(line)
+
+        print(f'Search Substring: {match_substring}')
+        print(f'in Section: {section}')
+        if exists:
+            if commented:
+                print(f'Uncommented')
+            else:
+                print(f'Already uncommented')
+        else:
+            print(f'Does not exist')
+        self.lines = updated_lines
+    
+    def replace_substring_in_value(self, section: str, key: str, match_substring: str, new_substring: str):
+        """
+        Replaces a substring in the value of a specific key in a given section.
+
+        :param section: The section to search in.
+        :param key: The key whose value needs to be updated.
+        :param match_substring: The substring to match in the current value.
+        :param new_substring: The new substring to replace the match.
+        """
+        in_section = False
+        exists = False
+        updated_lines = []
+        active_key = ''
+        active_value = ''
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_target_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and '=' in stripped and not stripped.startswith((';', '#')):
+                current_key, value = map(str.strip, stripped.split('=', 1))
+                if current_key == key and match_substring in value:
+                    value = value.replace(match_substring, new_substring)
+                    active_value = value
+                    active_key = current_key
+                    line = f'{current_key}={value}\n'
+                    exists = True
+
+            updated_lines.append(line)
+
+        print('Search Substring:', match_substring)
+        print(f'in Section: {section}')
+        print(f'in Key: {key}')
+
+        if not exists:
+            print('NOT FOUND')
+        else:
+            print('Search Substring Found.')
+            print('New record is as follows')
+            print(f'{active_key}={active_value}')
+        self.lines = updated_lines
+
+    def replace_substring_in_section(self, section: str, match_substring: str, new_substring: str):
+        """
+        Replaces a substring in the values of all keys within a given section.
+        
+        :param section: The section to search in.
+        :param match_substring: The substring to match in the current value.
+        :param new_substring: The new substring to replace the match.
+        """
+        in_section = False
+        exists = False
+        active_key = ''
+        active_value = ''
+        updated_lines = []
+
+        for line in self.lines:
+            stripped = line.strip()
+            if self._is_target_section(stripped, section):
+                in_section = True
+            elif stripped.startswith('[') and stripped.endswith(']'):
+                in_section = False
+            if in_section and '=' in stripped and not stripped.startswith((';', '#')):
+                current_key, value = map(str.strip, stripped.split('=', 1))
+                if match_substring in value:
+                    value = value.replace(match_substring, new_substring)
+                    active_value = value
+                    active_key = current_key
+                    line = f'{current_key}={value}\n'
+                    exists = True
+
+            updated_lines.append(line)
+
+        print('Search Substring:', match_substring)
+        print(f'in Section: {section}')
+
+        if not exists:
+            print('NOT FOUND')
+        else:
+            print('Search Substring Found.')
+            print('New record is as follows')
+            print(f'{active_key}={active_value}')
         self.lines = updated_lines
 
     def display(self):
